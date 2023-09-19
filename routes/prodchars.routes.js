ca59d472a173b42555a7a2d0807914fb8e52d3df
Vollmondo <dmitrifom74@gmail.com>
2023-09-19 17:01:38 +0500

    Update prodchars.routes.js

--- routes/prodchars.routes.js
+++ routes/prodchars.routes.js
@@ -7,17 +7,30 @@ const client = new MongoClient(uri, { useUnifiedTopology: true });
 const routerProdChars = express.Router();
 
 routerProdChars.get('/characteristics', async (req, res) => {
+    if (req.query.chars){
     try {
-      const chars = req.query.chars;
-  console.log(chars)
-      // Преобразование объекта chars в массив пар ключ-значение
-      const charEntries = Object.entries(chars);
-        
-      res.json(charEntries);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Internal server error' });
-    }
-  });
+    const chars = req.query.chars;
+    const db = client.db('react-app'); 
+    const collection = db.collection('Characteristics'); 
+    const charEntries = await Promise.all(Object.entries(chars).map(async ([key, value]) => {
+      const doc = await collection.findOne({ [key]: { $exists: true } });
+      if (doc) {
+        const { title, values } = doc[key];
+        return [title, values[value]];
+      } else {
+        return null;
+      }
+    }));
+
+    const validEntries = charEntries.filter(entry => entry !== null);
+
+    res.json(validEntries);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+}
+});
+
 
 module.exports = routerProdChars;
