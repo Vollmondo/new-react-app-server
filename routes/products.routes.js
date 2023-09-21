066bcd0e61f72434f40ffef9a6c70ea050a62487
Vollmondo <dmitrifom74@gmail.com>
2023-09-21 16:28:45 +0500

    added users favourite products feature

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -3,14 +3,13 @@ const { MongoClient, ObjectId } = require('mongodb');
 
 const uri = 'mongodb://localhost:27017';
 const client = new MongoClient(uri, { useUnifiedTopology: true });
+const database = client.db('react-app');
+const collection = database.collection('products');
 
 const routerProducts = express.Router();
 
 routerProducts.get('/products', async (req, res) => {
   try {
-    await client.connect();
-    const database = client.db('react-app');
-    const collection = database.collection('products');
     const data = await collection.find().toArray();
     res.json(data);
   } catch (error) {
@@ -20,39 +19,29 @@ routerProducts.get('/products', async (req, res) => {
 });
 
 routerProducts.get('/products/:id', async (req, res) => {
-    const { id } = req.params;
-    try {
-      await client.connect();
-      const database = client.db('react-app');
-      const collection = database.collection('products');
-      const product = await collection.findOne({ _id: new ObjectId(id) });
-      if (product) {
-        res.json(product);
-      } else {
-        res.status(401).send('Товара не существует');
-      }
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Ошибка выполнения запроса' });
-    } finally {
-      await client.close();
+  const { id } = req.params;
+  try {
+    const product = await collection.findOne({ _id: new ObjectId(id) });
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404).send('Товар не найден');
     }
-  });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Ошибка выполнения запроса' });
+  }
+});
 
 routerProducts.post('/products', async (req, res) => {
-    
-        try {
-          await client.connect();
-          const database = client.db('react-app');
-          const collection = database.collection('products');
-          const newData = req.body;
-          const result = await collection.insertOne(newData);
-          res.json(result);
-        } catch (error) {
-          console.error('Ошибка выполнения запроса:', error);
-          res.status(500).json({ error: 'Ошибка выполнения запроса' });
-        }
-
-    });
+  try {
+    const newData = req.body;
+    const result = await collection.insertOne(newData);
+    res.json(result);
+  } catch (error) {
+    console.error('Ошибка выполнения запроса:', error);
+    res.status(500).json({ error: 'Ошибка выполнения запроса' });
+  }
+});
 
 module.exports = routerProducts;
